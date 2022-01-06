592d591c36da1e0c91ade34e2f3bbc5ea17b1194
Reginaldo Dias <reginaldo.dias@flex.com>
2022-01-06 14:20:47 -0300

    ok

--- assets/js/dynamicapp.js
+++ assets/js/dynamicapp.js
@@ -23,6 +23,9 @@ function hideUserDetails() {
 }
 
 function showUsers(userspageId) {
+
+    alert("Entrou");
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
